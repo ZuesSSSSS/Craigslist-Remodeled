66b067f7b345323fda67c8b00b0155966cd8ca97
David Mudriy <davidmudriy@gmail.com>
2022-11-15 03:51:58 -0800

    Completed top nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,59 +1,14 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import './nav.css'
 
 function Nav() {
     return (
         <>
-            <nav id="top-nav">
-                <ul>
-                    <li>
-                        <Link to='/'>
-                            <a id='logo'>
-                                Craigslist
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/sign-up'>
-                            <a>
-                                Sign Up
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='login'>
-                            <a>
-                                Login
-                            </a>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <nav id="bottom-nav">
-                <ul>
-                    <li>
-                        <Link to='/community'>Community</Link>
-                    </li>
-                    <li>
-                        <Link to='/services'>Services</Link>
-                    </li>
-                    <li>
-                        <Link to='/forums'>Forums</Link>
-                    </li>
-                    <li>
-                        <Link to='/housing'>Housing</Link>
-                    </li>
-                    <li>
-                        <Link to='/for-sale'>For Sale</Link>
-                    </li>
-                    <li>
-                        <Link to='/jobs'>Jobs</Link>
-                    </li>
-                    <li>
-                        <Link to='/gigs'>Gigs</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="container">
+                <div className="Craigslist">Craigslist</div>
+                <button className="Login">Login</button>
+                <button className="Sign-Up">Sign Up</button>
+            </div>
         </>
     )
 }
